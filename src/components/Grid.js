02bb0ecb3cbd1be849794ae2c55798e6f744fa81
ngvuong <vminguyen@ucdavis.edu>
2021-11-18 22:16:39 -0800

    Update Grid component, save random ints in ref

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,26 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
 
 export default function Grid(props) {
   const [size, setSize] = useState(2);
+  const [isShuffled, setIsShuffled] = useState(false);
+  const randIntsRef = useRef();
   const randInts = [];
 
-  // useEffect(() => {
-  //   setSize(parseInt(props.level) + 1);
-  // }, [props.level]);
+  useEffect(() => {
+    setSize(parseInt(props.level) + 1);
+  }, [props.level]);
 
   while (randInts.length < size ** 2) {
-    const int = Math.floor(Math.random() * 10);
-    if (!(int in randInts)) {
+    const int = Math.floor(Math.random() * 256);
+    if (!randInts.includes(int)) {
       randInts.push(int);
     }
-    console.log(randInts, int);
   }
 
-  console.log(randInts);
+  randIntsRef.current = randInts;
+
   return (
     <div>
       {randInts.map((int) => {
+        return <Card key={int} index={int} />;
+      })}
+      {randIntsRef.current.map((int) => {
         return <Card key={int} index={int} />;
       })}
     </div>
