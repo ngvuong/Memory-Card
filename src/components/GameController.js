48cdb0e16662957e3913f02ef8bfd03cea3767ef
ngvuong <vminguyen@ucdavis.edu>
2021-11-22 23:07:31 -0800

    Move game logic to GameController

--- src/components/GameController.js
+++ src/components/GameController.js
@@ -19,12 +19,83 @@ export default function GameController() {
     console.log("fetched");
   }, []);
 
+  const [level, setLevel] = useState(1);
+  const [size, setSize] = useState(2);
+  const [isShuffled, setIsShuffled] = useState(false);
+  const randIntsRef = useRef();
+  const [randNums, setRandNums] = useState([]);
+  const randInts = randNums;
+  const [isClicked, setIsClicked] = useState({});
+  const isFirstRender = useRef(true);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    setSize(parseInt(level) + 1);
+    setRandNums([]);
+    console.log("set size");
+  }, [level]);
+
+  // useEffect(() => {
+  //   if (!skip) {
+  //     randIntsRef.current = [];
+  //   }
+  //   // setSkip(false);
+  // }, [skip, size]);
+
+  if (isShuffled) {
+    setIsShuffled(false);
+  }
+
+  while (randInts.length < size ** 2) {
+    const int = Math.floor(Math.random() * 256);
+    if (!randInts.includes(int)) {
+      randInts.push(int);
+      setIsClicked((prevState) => {
+        return { ...prevState, [int]: false };
+      });
+    }
+  }
+  randIntsRef.current = randInts;
+  useEffect(() => {
+    setRandNums(randInts);
+    // console.log(randNums);
+  }, [randInts]);
+  console.log(randIntsRef);
+
+  const onCardClick = (index) => {
+    for (let i = randIntsRef.current.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [randIntsRef.current[i], randIntsRef.current[j]] = [
+        randIntsRef.current[j],
+        randIntsRef.current[i],
+      ];
+    }
+    setIsClicked((prevState) => {
+      const newState = { ...prevState, [index]: true };
+      console.log(newState);
+      const allClicked = Object.keys(newState).every(
+        (key) => newState[key] === true
+      );
+      if (allClicked) {
+        console.log("all clicked");
+      }
+      return newState;
+    });
+    // console.log(isClicked);
+    // setSkip(false);
+    setIsShuffled(true);
+  };
+
   return (
     <div>
       {!isLoading && (
         <Grid
           countries={countriesRef.current}
           countryCodes={countryCodeRef.current}
+          randNums={randNums}
+          onCardClick={onCardClick}
         />
       )}
     </div>
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,65 +1,80 @@
 import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
 
-export default function Grid(props) {
-  const [size, setSize] = useState(2);
-  const [isShuffled, setIsShuffled] = useState(false);
-  const [skip, setSkip] = useState(true);
-  const levelRef = useRef();
-  const randIntsRef = useRef();
-  const [randNums, setRandNums] = useState([]);
-  const randInts = randNums;
-  const [isClicked, setIsClicked] = useState({});
-
+export default function Grid({
+  countries,
+  countryCodes,
+  randNums,
+  onCardClick,
+}) {
+  // const [size, setSize] = useState(2);
+  // const [isShuffled, setIsShuffled] = useState(false);
+  // const randIntsRef = useRef();
+  // const [randNums, setRandNums] = useState([]);
+  // const randInts = randNums;
+  // const [isClicked, setIsClicked] = useState({});
+  // const isFirstRender = useRef(true);
   // useEffect(() => {
+  //   if (isFirstRender.current) {
+  //     isFirstRender.current = false;
+  //     return;
+  //   }
   //   setSize(parseInt(props.level) + 1);
+  //   setRandNums([]);
   //   console.log("set size");
   // }, [props.level]);
 
-  // useEffect(() => {
-  //   if (!skip) {
-  //     randIntsRef.current = [];
+  // // useEffect(() => {
+  // //   if (!skip) {
+  // //     randIntsRef.current = [];
+  // //   }
+  // //   // setSkip(false);
+  // // }, [skip, size]);
+
+  // if (isShuffled) {
+  //   setIsShuffled(false);
+  // }
+
+  // while (randInts.length < size ** 2) {
+  //   const int = Math.floor(Math.random() * 256);
+  //   if (!randInts.includes(int)) {
+  //     randInts.push(int);
+  //     setIsClicked((prevState) => {
+  //       return { ...prevState, [int]: false };
+  //     });
   //   }
+  // }
+  // randIntsRef.current = randInts;
+  // useEffect(() => {
+  //   setRandNums(randInts);
+  //   // console.log(randNums);
+  // }, [randInts]);
+  // console.log(randIntsRef);
+
+  // const onCardClick = (index) => {
+  //   for (let i = randIntsRef.current.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [randIntsRef.current[i], randIntsRef.current[j]] = [
+  //       randIntsRef.current[j],
+  //       randIntsRef.current[i],
+  //     ];
+  //   }
+  //   setIsClicked((prevState) => {
+  //     const newState = { ...prevState, [index]: true };
+  //     console.log(newState);
+  //     const allClicked = Object.keys(newState).every(
+  //       (key) => newState[key] === true
+  //     );
+  //     if (allClicked) {
+  //       console.log("all clicked");
+  //     }
+  //     return newState;
+  //   });
+  //   // console.log(isClicked);
   //   // setSkip(false);
-  // }, [skip, size]);
-  console.log(props.level);
+  //   setIsShuffled(true);
+  // };
 
-  if (props.level !== levelRef.current) {
-    randIntsRef.current = [];
-  }
-  levelRef.current = props.level;
-  if (isShuffled) {
-    setIsShuffled(false);
-  }
-
-  while (randInts.length < size ** 2) {
-    const int = Math.floor(Math.random() * 256);
-    if (!randInts.includes(int)) {
-      randInts.push(int);
-      setIsClicked((prevState) => {
-        return { ...prevState, [int]: false };
-      });
-    }
-  }
-  randIntsRef.current = randInts;
-  useEffect(() => {
-    setRandNums(randInts);
-    console.log(randNums);
-  }, [randInts]);
-  console.log(randIntsRef);
-  const onCardClick = (index) => {
-    for (let i = randIntsRef.current.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [randIntsRef.current[i], randIntsRef.current[j]] = [
-        randIntsRef.current[j],
-        randIntsRef.current[i],
-      ];
-    }
-    setIsClicked({ ...isClicked, [index]: true });
-    console.log(isClicked);
-    // setSkip(false);
-    setIsShuffled(true);
-  };
   return (
     <React.Fragment>
       {randNums.map((int) => {
@@ -68,8 +83,8 @@ export default function Grid(props) {
             key={int}
             index={int}
             onClick={onCardClick}
-            countries={props.countries}
-            countryCodes={props.countryCodes}
+            countries={countries}
+            countryCodes={countryCodes}
           />
         );
       })}
