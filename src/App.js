2beb25bb097410b9905e387c923884d3250b732e
ngvuong <vminguyen@ucdavis.edu>
2021-11-24 02:58:04 -0800

    Clean up app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-// import React, { useState, useEffect, useRef } from "react";
-// import Card from "./components/Card";
-// import Grid from "./components/Grid";
+import React from "react";
 import GameController from "./components/GameController";
 import "./styles/App.css";
 
 function App() {
-  // const countriesRef = useRef();
-  // const countryCodeRef = useRef();
-  // const [isLoading, setIsLoading] = useState(true);
-  // let randInt;
-  // if (!isLoading) {
-  //   randInt = Math.floor(Math.random() * 256);
-  // }
-  // useEffect(() => {
-  //   fetch("https://flagcdn.com/en/codes.json").then((res) => {
-  //     res.json().then((data) => {
-  //       countriesRef.current = JSON.parse(JSON.stringify(data));
-  //       countryCodeRef.current = Object.keys(countriesRef.current).filter(
-  //         (key) => !key.includes("us-")
-  //       );
-  //       setIsLoading(false);
-  //     });
-  //   });
-  // }, []);
-
-  // const onRandomize = () => {
-  //   setIsLoading(true);
-  //   setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 100);
-  // };
-
   return (
     <div className="App">
       <h1 className="title">Memory Master</h1>
